4b0ffde5838362ccd49568e49c39eb529a2cc982
Ndaru <muhamad.ndaru@gmail.com>
2022-11-08 21:49:13 +0700

    edit file

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,8 +1,9 @@
 @extends('app')
-@section('title', 'Daftar Buku - PT. Mentol')
+@section('title', 'Daftar Buku - Ndaruhes')
 
 @section('content')
-    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);"
+        aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="#" class="text-dark text-decoration-none">Home</a></li>
             <li class="breadcrumb-item active" aria-current="page">Books</li>
@@ -23,7 +24,7 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach($books as $book)
+                @foreach ($books as $book)
                     <tr>
                         <th scope="row">{{ $loop->iteration }}</th>
                         <td>{{ $book->judul_buku }}</td>
@@ -31,8 +32,11 @@
                         <td>{{ $book->jumlah_halaman_buku }}</td>
                         <td>{{ $book->tahun_terbit_buku }}</td>
                         <td>
-                            <a href="{{route('editBook', $book->id)}}" class="btn btn-primary btn-sm"><i class="fas fa-pencil-alt"></i></a>
-                            <a href="#" data-uri="{{route('deleteBook', $book->id)}}" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal"><i class="fas fa-trash-alt"></i></a>
+                            <a href="{{ route('editBook', $book->id) }}" class="btn btn-primary btn-sm"><i
+                                    class="fas fa-pencil-alt"></i></a>
+                            <a href="#" data-uri="{{ route('deleteBook', $book->id) }}" class="btn btn-danger btn-sm"
+                                data-bs-toggle="modal" data-bs-target="#confirmDeleteModal"><i
+                                    class="fas fa-trash-alt"></i></a>
                         </td>
                     </tr>
                 @endforeach
